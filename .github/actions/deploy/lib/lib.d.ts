021c820ff5649bdd84e6c40aeb04e3179a239ee8
Jarvis <418536538@qq.com>
2020-12-11 09:52:50 +0800

    test: gh-pages-test on Updated actions

---
+++ .github/actions/deploy/lib/lib.d.ts
@@ -0,0 +1,8 @@
+import {ActionInterface, NodeActionInterface} from './constants'
+/** Initializes and runs the action.
+ *
+ * @param {object} configuration - The action configuration.
+ */
+export default function run(
+  configuration: ActionInterface | NodeActionInterface
+): Promise<void>
